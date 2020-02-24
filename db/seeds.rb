e74249af1caa1e2f30d3f468d515663aef28dd39
Nicolas Bouygues <bouygues.nicolas@gmail.com>
2020-02-24 18:07:58 +0100

    user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+# TODO: Write a seed to insert 100 posts in the database
+
+puts 'Creating 10 fake users...'
+10.times do
+  user = User.new(username: Faker::Internet.username, email: Faker::Internet.email, password: Faker::Internet.password, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  user.save!
+end
+puts 'Finished!'
