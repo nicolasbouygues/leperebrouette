e4894cbe349ad704f91c2eb9f601d272c23733d3
arthurleriche <lerichearthur@gmail.com>
2020-02-27 15:01:06 +0100

    add myboxe

--- app/controllers/barrows_controller.rb
+++ app/controllers/barrows_controller.rb
@@ -2,6 +2,10 @@ class BarrowsController < ApplicationController
   before_action :set_barrow, only: [:show, :edit, :update, :destroy]
   skip_before_action :authenticate_user!, only: [:index, :show]
 
+  def town
+  [town, city, state, country].compact.join(', ')
+  end
+
   # GET /barrows
   # GET /barrows.json
   def index
@@ -15,6 +19,15 @@ class BarrowsController < ApplicationController
       @barrows = Barrow.all
     end
 
+    @Barrows_coordinates = Barrow.geocoded
+
+      @markers = @Barrows_coordinates.map do |barrow|
+        {
+          lat: barrow.latitude,
+          lng: barrow.longitude
+        }
+      end
+
     # authorize @barrows
     # @barrows = Barrow.where(town: param[:town] AND category: params[:category])
     # @barrows = policy_scope(Barrow).order(created_at: :desc)
@@ -24,6 +37,13 @@ class BarrowsController < ApplicationController
   def my_barrows
     @barrows = Barrow.where(user_id: current_user.id)
     @bookings = Booking.where(user_id: current_user.id)
+    @Barrows_coordinates = Barrow.geocoded.where(user_id: current_user.id)
+    @markers = @Barrows_coordinates.map do |barrow|
+        {
+          lat: barrow.latitude,
+          lng: barrow.longitude
+        }
+      end
     # authorize @barrows
     # authorize @bookings
   end
@@ -31,6 +51,14 @@ class BarrowsController < ApplicationController
   # GET /barrows/1.json
   def show
     @booking = Booking.new
+
+    @Barrows_coordinates = Barrow.geocoded.where(user_id: current_user.id)
+    @markers = @Barrows_coordinates.map do |barrow|
+        {
+          lat: barrow.latitude,
+          lng: barrow.longitude
+        }
+      end
   end
 
   # GET /barrows/new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_135316) do
+ActiveRecord::Schema.define(version: 2020_02_27_133845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_02_26_135316) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "booking_date"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_barrows_on_user_id"
   end
 
